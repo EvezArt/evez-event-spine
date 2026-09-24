e947b36d612b3b4d0ee3b11ac4cc2440096df6b6
Steven Crawford-Maggard <129563238+EvezArt@users.noreply.github.com>
2026-09-24 14:46:30 -0700

    feat: expose guarded executor API

--- src/evez_event_spine/__init__.py
+++ src/evez_event_spine/__init__.py
@@ -1,4 +1,6 @@
-"""EVEZ Event Spine."""
+"""EVEZ Event Spine public API."""
+
 from .spine import *  # noqa
 from .file_spine import FileEventSpine, canonical_event_bytes, event_hash  # noqa
 from .reality_kernel import *  # noqa
+from .gateway import ExecutionReceipt, GuardedExecutor  # noqa
